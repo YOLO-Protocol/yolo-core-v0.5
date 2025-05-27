101a848175f144e40888a0d46b8cbeb7c77917ea
Alvin Yap <leaf.alvin@gmail.com>
2025-05-28 03:40:16 +0800

    feat(IYoloOracle): add IYoloOracle

---
+++ src/interfaces/IYoloOracle.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+interface IYoloOracle {
+    function anchor() external returns (address);
+
+    function setAssetSources(address[] calldata assets, address[] calldata sources) external;
+
+    function getAssetPrice(address asset) external view returns (uint256);
+
+    function getAssetsPrices(address[] calldata assets) external view returns (uint256[] memory);
+
+    function getSourceOfAsset(address asset) external view returns (address);
+}
