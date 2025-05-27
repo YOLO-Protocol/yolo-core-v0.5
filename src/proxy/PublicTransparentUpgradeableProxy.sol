0e80f5393726470687068045ce49f5f6f3d365c3
Alvin Yap <leaf.alvin@gmail.com>
2025-05-27 19:49:24 +0800

    feat(proxy)L add PublicTransparentUpgradeableProxy as an extension to expose proxy state

---
+++ src/proxy/PublicTransparentUpgradeableProxy.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.29;
+
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {IVersionized} from "@yolo/contracts/interfaces/Iversionized.sol";
+
+/**
+ * @title   PublicTransparentUpgradeableProxy
+ * @author  0xyolodev.eth
+ * @dev     Extends OpenZeppelin's TransparentUpgradeableProxy to allow public reading of
+ *          the implementation address, admin address and the implementation version.
+ */
+contract PublicTransparentUpgradeableProxy is TransparentUpgradeableProxy {
+    /**
+     * @dev Constructor that initializes the proxy with the implementation address, admin address, and initialization data.
+     * @param _logic The address of the initial implementation contract.
+     * @param _admin The address of the proxy admin.
+     * @param _data Initialization data to be passed to the implementation contract.
+     */
+    constructor(address _logic, address _admin, bytes memory _data)
+        TransparentUpgradeableProxy(_logic, _admin, _data)
+    {}
+
+    function implementation() public view returns (address) {
+        return _implementation();
+    }
+
+    function proxyAdmin() public view returns (address) {
+        return _proxyAdmin();
+    }
+
+    function version() public view returns (string memory) {
+        return IVersionized(_implementation()).version();
+    }
+}
